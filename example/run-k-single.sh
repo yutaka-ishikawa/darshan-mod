fe3c6dc40704868b855ef00bff1b71f7699b9d89
Yutaka Ishikawa <yutaka.ishikawa@riken.jp>
2018-07-19 20:43:33 +0900

    DIST-binary, DIST-source, example/run-k-mpi.sh, example/run-k-single.sh

---
+++ example/run-k-single.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#PJM --rsc-list rscgrp=small
+#PJM --rsc-list node=1
+#PJM --rsc-list elapse=0:10:00  # HH:MM:SS
+#PJM --mpi use-rankdir
+#
+#PJM --stgin "./simple-single ./"
+#PJM --stgin "../k-lib/libdarshan-single.so ./"
+#PJM --stgout "./darshanlog-* ./results/"  
+#
+
+. /work/system/Env_base
+
+(export LD_PRELOAD=./libdarshan-single.so; export DARSHAN_HISTORY_RW=rw; ./simple-single wdata2)
